d3908f75954bad5567090b2db1994443115c924a
Harsha5e5 <31989540+Harsha5e5@users.noreply.github.com>
2017-09-16 15:09:07 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-	
+		
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
+Student st = new Student(01,"Harikrihna",new Date(1996/01/06),63.568);
+		Student st2 = new Student(02,"KISHORE",new Date(1996/01/06),63.568);
+		Student st3 = new Student(03,"KISHORE",new Date(1996/01/06),63.568);
+		Student st4 = new Student(04,"KISHORE",new Date(1996/01/06),63.568);
+		
 	}
 
 }
